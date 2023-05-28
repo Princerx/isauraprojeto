80596bd884d52c1101ae3b1a32c18555c1644b57
Princerx <114414534+Princerx@users.noreply.github.com>
2023-05-28 19:11:06 -0300

    isaurarv

---
+++ java.js
@@ -0,0 +1,52 @@
+function vacina() {
+
+    document.getElementById('1').innerHTML = "BCG HEPATITE B";
+}
+
+function vacina2() {
+    document.getElementById('2').innerHTML = "Penta/DTP, Vip/Vop, Pneumocócica, RotaVirus";
+}
+
+function vacina3() {
+    document.getElementById('3').innerHTML = "Meningocócica c";
+}
+
+function vacina4() {
+    document.getElementById('4').innerHTML = "Penta/DTP, Vip/Vop, Pneumocócica, RotaVirus";
+}
+
+function vacina5() {
+    document.getElementById('5').innerHTML = "Meningocócica c";
+}
+
+function vacina6() {
+    document.getElementById('6').innerHTML = "Penta/DTP, Vip/Vop";
+}
+
+function vacina7() {
+    document.getElementById('7').innerHTML = "Febre Amarela";
+}
+
+function vacina8() {
+    document.getElementById('8').innerHTML = "Pneumocócica, Meningocócica c, TrípliceViral ";
+}
+
+function vacina9() {
+    document.getElementById('9').innerHTML = "Penta/DTP, Vip/Vop, Hepatite A, TetraViral";
+}
+
+function vacina10() {
+    document.getElementById('10').innerHTML = "Penta/DTP, Vip/Vop, Varicela";
+}
+
+function vacina11() {
+    document.getElementById('11').innerHTML = "HPV";
+}
+
+function vacina12() {
+    document.getElementById('12').innerHTML = "HPV";
+}
+
+function vacina13() {
+    document.getElementById('13').innerHTML = "Hepatite B, Febre Amarela, DT, TripliceViral, Pneumocócica";
+}
