62a07e56026f229e59f84054f0093079f2bf1bdf
Princerx <114414534+Princerx@users.noreply.github.com>
2023-05-29 23:42:21 -0300

    commit final

--- java.js
+++ java.js
@@ -1,52 +1,155 @@
 function vacina() {
+    var botao = document.getElementById('1');
 
-    document.getElementById('1').innerHTML = "BCG HEPATITE B";
+    if (botao.innerHTML === "Ao Nascer") {
+        botao.innerHTML = "BCG HEPATITE B";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "Ao Nascer";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina2() {
-    document.getElementById('2').innerHTML = "Penta/DTP, Vip/Vop, Pneumocócica, RotaVirus";
+    var botao = document.getElementById('2');
+
+    if (botao.innerHTML === "2 Meses") {
+        botao.innerHTML = "Penta/DTP, Vip/Vop, Pneumocócica, RotaVirus";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "2 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina3() {
-    document.getElementById('3').innerHTML = "Meningocócica c";
+    var botao = document.getElementById('3');
+
+    if (botao.innerHTML === "3 Meses") {
+        botao.innerHTML = "Meningocócica c";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "3 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina4() {
-    document.getElementById('4').innerHTML = "Penta/DTP, Vip/Vop, Pneumocócica, RotaVirus";
+    var botao = document.getElementById('4');
+
+    if (botao.innerHTML === "4 Meses") {
+        botao.innerHTML = "Penta/DTP, Vip/Vop, Pneumocócica, RotaVirus";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "4 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina5() {
-    document.getElementById('5').innerHTML = "Meningocócica c";
+    var botao = document.getElementById('5');
+
+    if (botao.innerHTML === "5 Meses") {
+        botao.innerHTML = "Meningocócica c";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "5 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina6() {
-    document.getElementById('6').innerHTML = "Penta/DTP, Vip/Vop";
+    var botao = document.getElementById('6');
+
+    if (botao.innerHTML === "6 Meses") {
+        botao.innerHTML = "Penta/DTP, Vip/Vop";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "6 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina7() {
-    document.getElementById('7').innerHTML = "Febre Amarela";
+    var botao = document.getElementById('7');
+
+    if (botao.innerHTML === "9 Meses") {
+        botao.innerHTML = "Febre Amarela";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "9 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina8() {
-    document.getElementById('8').innerHTML = "Pneumocócica, Meningocócica c, TrípliceViral ";
+    var botao = document.getElementById('8');
+
+    if (botao.innerHTML === "12 Meses") {
+        botao.innerHTML = "Pneumocócica, Meningocócica c, TrípliceViral";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "12 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina9() {
-    document.getElementById('9').innerHTML = "Penta/DTP, Vip/Vop, Hepatite A, TetraViral";
+    var botao = document.getElementById('9');
+
+    if (botao.innerHTML === "15 Meses") {
+        botao.innerHTML = "Penta/DTP, Vip/Vop, Hepatite A, TetraViral";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "15 Meses";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina10() {
-    document.getElementById('10').innerHTML = "Penta/DTP, Vip/Vop, Varicela";
+    var botao = document.getElementById('10');
+
+    if (botao.innerHTML === "4 Anos") {
+        botao.innerHTML = "Penta/DTP, Vip/Vop, Varicela";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "4 Anos";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina11() {
-    document.getElementById('11').innerHTML = "HPV";
+    var botao = document.getElementById('11');
+
+    if (botao.innerHTML === "Meninos 11/14 Anos") {
+        botao.innerHTML = "HPV";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "Meninos 11/14 Anos";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina12() {
-    document.getElementById('12').innerHTML = "HPV";
+    var botao = document.getElementById('12');
+
+    if (botao.innerHTML === "Meninas 09/14 Anos") {
+        botao.innerHTML = "HPV";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "Meninas 09/14 Anos";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
 
 function vacina13() {
-    document.getElementById('13').innerHTML = "Hepatite B, Febre Amarela, DT, TripliceViral, Pneumocócica";
+    var botao = document.getElementById('13');
+
+    if (botao.innerHTML === "11/19 Anos") {
+        botao.innerHTML = "Hepatite B, Febre Amarela, DT, TripliceViral, Pneumocócica";
+        botao.style.backgroundColor = "#052077"; // Altera a cor para verde
+    } else {
+        botao.innerHTML = "11/19 Anos";
+        botao.style.backgroundColor = "rgb(11, 100, 128)"; // Restaura a cor original
+    }
 }
